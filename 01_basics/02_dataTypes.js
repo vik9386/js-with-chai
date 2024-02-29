a14b96ca5fde0fb860316c1b92ff14571d711e8e
vik9386 <129299389+vik9386@users.noreply.github.com>
2024-02-29 14:34:14 +0000

    dataypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict";//treat as newer version of javascript
+let age=15
+// alert (3+3)// becoz we are using node not browser
+
+
+// number =>  range- 2 to power 53 
+// bigInt 
+// string =>""
+// null => empty or stadalone value
+// undefined=>not assigned now
+// symbols= used for finding uniqueness (used in React.js)
+
+
+
+
+
+
+console.log(typeof age)
+console.log(typeof "vikash")
+console.log(typeof null) // object (its a bug in JS )
+console.log(typeof undefined) // undefined
