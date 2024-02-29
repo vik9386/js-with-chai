b592420d721b1c69d68e41cbd61b1c0a8b5ca0cb
vik9386 <129299389+vik9386@users.noreply.github.com>
2024-02-29 15:06:53 +0000

    conversion of datatypes

---
+++ 01_basics/03_converionOperation.js
@@ -0,0 +1,30 @@
+let age=33
+let bool=Number(true)
+let und=Number(undefined)
+let nul=Number(null)
+
+
+// console.log(typeof age)
+// console.log(typeof (age))
+
+let numb=Number("33abc")
+
+//console.log(typeof numb) //number
+//console.log(numb)// NaN
+//console.log(bool)// 1
+//console.log(und)//Nan
+//console.log(nul)//0
+
+let isLoggeedIn= "";
+
+let booleanIsLoggedIn = Boolean(isLoggeedIn)
+// console.log(booleanIsLoggedIn)
+
+
+// 1=>true,0=>false
+//"" (empty string) =>false
+// "vikash"=>true
+
+let str=33
+// console.log(typeof Number(str))
+// console.log(str)
