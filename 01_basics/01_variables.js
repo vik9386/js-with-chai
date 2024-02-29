5be1012069796dde722a2cb6bcdef066cdef9a2e
vik9386 <129299389+vik9386@users.noreply.github.com>
2024-02-29 14:02:14 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accoountId=234571;
+let accountEmail = "vikash234@gmail.com"
+var accountPassword = "1237423"
+accountCity ="jaipur"
+let accountState // undefined
+
+
+// accoountId =2 // not allowed
+
+accountEmail = "vikash1234@gmail.com"
+accountPassword = "2121212121"
+accountCity ="bengaluru"
+
+// console.log(accoountId)
+/* prefer not to use var 
+bcoz block scope & functional scope 
+*/
+
+console.table([accoountId,accountEmail,accountCity,accountPassword,accountState]);
