c3fb8bf9b6e85d32ee734d9df9316f2676ba451c
vik9386 <129299389+vik9386@users.noreply.github.com>
2024-02-29 18:53:35 +0000

    maths and nums

---
+++ 01_basics/06_numa_and_maths.js
@@ -0,0 +1,31 @@
+let score =100
+// console.log(score)
+
+const balance=new Number(400.12395)
+// console.log(balance)
+
+// console.log(score.toString().length)
+
+// console.log(balance.toFixed(2))
+// console.log(balance.toPrecision(4))
+
+const hundreds =1000000
+// console.log(hundreds.toLocaleString("en-IN"))
+
+
+
+//+++++++++++++ Maths ++++++++++++
+
+// console.log(Math.abs(-4))
+// console.log(Math.random());
+// console.log(Math.floor(Math.random()*10)+1)
+
+
+let min=1
+let max=6
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
+
+
+
+
