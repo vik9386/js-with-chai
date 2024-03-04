9a7cab84b819f2bff3dbf52d93976602d971c925
vik9386 <129299389+vik9386@users.noreply.github.com>
2024-03-04 19:13:42 +0000

    basic array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,50 @@
+const myArr=[0,1,2,3,4,5]
+// console.log(myArr)
+
+const heroes=['shaktiman','naagraj']
+// console.log(myArr[1])
+
+// Array Methods
+// myArr.push(6)
+// myArr.push(7)
+// console.log(myArr)
+
+// myArr.pop()
+// console.log(myArr)
+
+// myArr.unshift(9)
+// console.log(myArr)
+
+// myArr.shift()
+// console.log(myArr)
+
+
+// console.log(myArr.includes(3))
+// console.log(myArr.indexOf(3))
+
+
+// const newArr=myArr.join()
+// console.log(myArr)
+// console.log(newArr)
+// console.log(typeof newArr)// string to join method converts array into string separated by comma
+
+
+// slice,splice
+
+// const myNewn1=myArr.slice(1,3)// return a copy of sections as an array
+// console.log(myArr)
+// console.log(myNewn1)
+
+// const myNewn2= myArr.splice(1,3)//retrun deleted element as ana array
+// console.log(myArr)
+// console.log(myNewn2)
+
+// slice method original array ko change nhi krta h jbki splice method original array ko disturb krta h
+
+
+
+
+
+
+
+
