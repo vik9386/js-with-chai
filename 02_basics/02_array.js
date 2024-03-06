3b091b6eb2b9b485f766ef7a6ae2a521edf730aa
vik9386 <129299389+vik9386@users.noreply.github.com>
2024-03-06 16:14:11 +0000

    arrays

---
+++ 02_basics/02_array.js
@@ -0,0 +1,35 @@
+const marvelheroes=["spiderman","thor","ironman"]
+const dcHeroes=["superman","flash","batman"]
+
+// marvelheroes.push(dcHeroes)
+// console.log(marvelheroes)
+
+// const newArray = marvelheroes.concat(dcHeroes)
+// console.log(marvelheroes)
+// console.log(newArray)
+
+
+// spread operator
+
+// const allHeroes = [...marvelheroes,...dcHeroes]
+// console.log(allHeroes)
+
+// console.log(Array.isArray(marvelheroes))
+// console.log(Array.from("hitesh"))// converting to an array
+
+console.log(Array.from({name:"vikash"}))//interesting please prepare for interview 
+
+
+// making array flat
+
+// const anotherArray=[1,2,3,[4,5,8],3,6,[2,10,8,[2,6,8]]]
+
+// const againArray =anotherArray.flat(Infinity)
+// console.log(againArray)
+
+// let score1=100
+// let score2=200
+// let score3=300
+// console.log(Array.of(score1,score2,score3))
+
+
