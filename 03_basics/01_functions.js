62b40757bb60f7ab905508b6029167ab4542d264
vik9386 <129299389+vik9386@users.noreply.github.com>
2024-03-07 14:38:59 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,64 @@
+// function addTwoNumbers (number1,number2){
+//     return number1+number2
+// }
+
+// addTwoNumbers // without () it is reference of function
+
+// addTwoNumbers(2,4)// with () it is exection of function or function call 
+
+// number1 and number2 are parameter
+// 2,3 are argument
+
+// const result = addTwoNumbers(2,3)
+// console.log(result)
+
+// default parameter
+
+// function loginUserMessage(userName ="sam"){
+//     return `${userName} just logged in`
+// }
+
+// console.log(loginUserMessage("hitesh"))
+
+
+// function calculateCartPrice(val1,val2,...nums){
+//     let sum = 0
+//    nums.map(num=>{
+//     sum = sum + num
+//    })
+
+//    return sum+val1+val2
+// }
+
+// console.log(calculateCartPrice(100,200,300,400,500))
+
+// const user={
+//     userName:"vikash",
+//     price:199
+// }
+
+// function handleObject (anyObject){
+//     console.log(`user name is ${anyObject.userName} and price is ${anyObject.price}`)
+// }
+
+// handleObject(user)
+
+// we can pass directly object
+
+// handleObject({
+//     userName:"sam",
+//     price:399
+// })
+
+// const myNewArray = [100,200,400,800]
+
+// function returnSecondValue(getArray){
+//     return getArray[1]
+// }
+
+// console.log(returnSecondValue(myNewArray))
+
+// we can pass array directly
+
+// console.log(returnSecondValue([100,200,300,400,300]))
+
