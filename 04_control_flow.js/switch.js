24b1aa822a49fa94616d8e0dc91faad79dc1c659
vik9386 <129299389+vik9386@users.noreply.github.com>
2024-03-08 09:54:34 +0000

    switch

---
+++ 04_control_flow.js/switch.js
@@ -0,0 +1,45 @@
+const month=3
+
+switch(month){
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("february");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break; 
+    case 5:
+        console.log("may");
+        break;
+    case 6:
+        console.log("june");
+        break;
+    case 7:
+        console.log("july");
+        break;
+    case 8:
+        console.log("august");
+        break;
+    case 9:
+        console.log("september");
+        break;
+    case 10:
+        console.log("october");
+        break;
+    case 11:
+        console.log("november");
+        break;
+    default:
+        console.log("december")
+        break;    
+}
+
+
+// if any where break is missing then after matching 
+// of case , uske niche ka sara code execute ho jta h
+// except default case
