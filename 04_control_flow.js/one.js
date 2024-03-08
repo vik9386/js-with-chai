a58cbb92e353ed925e50f7cb85d3d7538e31f2cc
vik9386 <129299389+vik9386@users.noreply.github.com>
2024-03-08 09:32:38 +0000

    if else

---
+++ 04_control_flow.js/one.js
@@ -0,0 +1,23 @@
+let score = 200
+
+// if(score >= 200) console.log("good")
+
+// if(score >=200){
+//     const power="fly"
+//     console.log(`new power is ${power}`)
+// }else{
+//    console.log(`no power`)
+// }
+
+
+let balance =1000
+
+if(balance < 500){
+    console.log('less than 500')
+}else if(balance < 750){
+    console.log('less than 750')
+}else if(balance < 900){
+    console.log('balance les than 900')
+}else{
+    console.log(`balance is ${balance}`)
+}
