a9df667584d3bd55bb68053c49269c10dffc5472
vik9386 <129299389+vik9386@users.noreply.github.com>
2024-03-08 18:07:11 +0000

    forEach

--- 05_iterations.js/five.js
+++ 05_iterations.js/five.js
@@ -11,3 +11,31 @@ const coding=["js","cpp","ruby","swift","java"]
 // }
 
 // coding.forEach(printMe)
+
+// forEach syntax --- forEach((element,index,arr)=>{})
+
+// coding.forEach((item,index,arr)=>{
+//     console.log(`index is ${index} and value is ${item} and array is ${arr}`)
+// })
+
+// array of objects --> [{},{},{}]
+
+const myCoding=[
+    {
+        language:"javascript",
+        extension:"js"
+    },
+    {
+        language:"python",
+        extension:"py"
+    },
+    {
+        language:"java",
+        extension:"java"
+    },
+]
+
+myCoding.forEach((item)=>{
+    console.log(item.language)
+})
+
