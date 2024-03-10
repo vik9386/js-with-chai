e5b64214e8ae826d99cfe0abef97653579a6e1d2
vik9386 <129299389+vik9386@users.noreply.github.com>
2024-03-10 11:40:38 +0000

    project modification

--- 07_projects/color changer/script.js
+++ 07_projects/color changer/script.js
@@ -5,6 +5,19 @@ buttons.forEach(function(button){
     button.addEventListener("click",function(e){
         // console.log(e)
         // console.log(e.target.id)
-        body.style.backgroundColor=e.target.id
+        switch(e.target.id){
+            case "grey":
+                body.style.backgroundColor=e.target.id
+                break;
+            case "red":
+                body.style.backgroundColor=e.target.id
+                break;
+            case "blue":
+                body.style.backgroundColor=e.target.id
+                break;
+            default:    
+            body.style.backgroundColor=e.target.id
+            break;
+        }
     })
 })
