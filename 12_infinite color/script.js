16084074bcb5360a16ffeb04e889b81aabce6b65
vik9386 <129299389+vik9386@users.noreply.github.com>
2024-03-14 17:06:10 +0000

    projects

---
+++ 12_infinite color/script.js
@@ -0,0 +1,31 @@
+const randomColor = function () {
+    const hex = "0123456789abcdef";
+    let color = "#";
+    for(i=0;i<6;i++){
+      color +=hex[Math.floor(Math.random()*16)] 
+    }
+    return color
+  };
+  
+  let intervalId
+  const startChangingColor=function(){
+    if(!intervalId){
+      intervalId=setInterval(changeBgColor,1000)
+    }
+      
+      function changeBgColor (){
+          document.body.style.backgroundColor=randomColor()
+      } 
+  }
+  
+  const stopChangingColor=function(){
+      clearInterval(intervalId)
+      intervalId = null
+  }
+  
+   document.getElementById("start").addEventListener('click',startChangingColor)
+   document.getElementById("stop").addEventListener('click',stopChangingColor)
+  
+  
+  
+  
