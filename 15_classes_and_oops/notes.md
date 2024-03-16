2e746f8f713065d1c524d8d1ebe2a2c7f11c63d9
vik9386 <129299389+vik9386@users.noreply.github.com>
2024-03-16 08:37:57 +0000

    notes and keyword

---
+++ 15_classes_and_oops/notes.md
@@ -0,0 +1,20 @@
+# javascript and classes 
+
+## javazscipt is prototype based prog. language
+## classes 
+- collection of properties and methods
+
+## why use oop
+object literal
+
+-construction function
+-prototypes
+-classes
+-Instances(new,this)
+
+
+# 4 pillars 
+Abstraction
+Encapsulation
+Inheritance
+Polymorphism
---
+++ 15_classes_and_oops/oop.js
@@ -0,0 +1,29 @@
+// const user = {
+//     userName:"vikash",
+//     logInCount:8,
+//     signedIn:true,
+
+//     userDetails: function(){
+//         // console.log(`got user details from database`)
+//         console.log(`username: ${this.userName}`)
+//         console.log(this)
+//     }
+
+// }
+
+// console.log(user.userName)
+// console.log(user.userDetails())
+// console.log(this)
+
+
+function User(username,loginCount,isLoggedIn){
+    this.username=username;
+    this.loginCount =loginCount;
+    this.isLoggedIn=isLoggedIn
+    return this
+}
+// new keyword hi constructor kehlata h or ye naya instance deta h hmesa
+const userOne = new User("vikash",12,true)
+const userTwo = new User("akash",11,false)
+console.log(userOne) 
+console.log(userTwo)
